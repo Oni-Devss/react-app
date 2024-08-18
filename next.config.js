e94608ec03bfaa41a844c214e5923da606996fb8
Ash <57202360+MalikeChapman@users.noreply.github.com>
2024-08-18 19:26:29 -0400

    changed gh config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 const plugins = [
